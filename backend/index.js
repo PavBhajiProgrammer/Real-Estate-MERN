88f9424d8c5e7bfa0a9845ee152cecc153731b56
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-10-17 13:06:33 +0530

    changes in file structure

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,28 @@
-const express = require("express");
-const { default: mongoose } = require("mongoose");
-require("dotenv").config();
+// const express = require("express");
+import express from "express";
+// const { default: mongoose } = require("mongoose");
+// import mongoose from "mongoose";
+// require("dotenv").config();
+// import dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5010;
 
 // connecting with database
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING)
-  .then(() => {
-    console.log("connedted to database");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(process.env.MONGODB_CONNECTION_STRING)
+//   .then(() => {
+//     console.log("connedted to database");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
 });
 
-app.get("/", () => {
-  console.log("heloo there");
+app.get("/", (req, res) => {
+  res.send("heloo there");
 });
